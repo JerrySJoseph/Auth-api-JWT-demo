ceb8e525ad7e3a37d2773f68dc5963c78f19c73b
Jerry S Joseph <jerin.sebastian153@gmail.com>
2021-01-25 11:45:42 +0530

    new commit

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 var express=require('express')
+var mongoose = require('mongoose')
+const dotenv=require('dotenv');
+
 var app=express();
+dotenv.config();
+
 
 const PORT = 3000 || process.env.PORT
+
+
+//Connecting to Database
+mongoose.connect(process.env.DB_CONNECTION_STRING,
+    {useNewUrlParser:true,useUnifiedTopology:true},
+    (error)=>{
+    if(error)
+    console.log('Error connecting to Database :'+error.errmsg)
+    else
+    console.log("Connection Established to Database!")
+})
 
 //Importing Routes
 const authRouter=require('./routes/auth')
@@ -9,6 +25,5 @@ const authRouter=require('./routes/auth')
 //Routing to Routes
 app.use('/api/auth',authRouter)
 
-
-
+//Listening to PORT 3000 or process.env.PORT at localhost
 app.listen(PORT,()=>console.log("Auth Server is Up and Running on localhost:"+PORT));
